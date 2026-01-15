23ec3ff0024f6a95d4a443e18d1f759a5bb7c850
User Name <usermail@gmail.com>
2026-01-15 11:35:04 +0530

    add interface for adding new habbit

---
+++ static/auth.js
@@ -0,0 +1,14 @@
+const habitList = document.getElementById('habitList');
+const add = document.getElementById('addNewHabit');
+
+add.addEventListener('click', function(){
+    
+})
+
+function openModal(){
+    document.getElementById('habitModal').style.display = 'flex';
+}
+
+function closeModal(){
+    document.getElementById('habitModal').style.display = 'none';
+}
