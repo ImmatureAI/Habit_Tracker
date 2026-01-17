07700d6a6d8466f07a07d12bcea7fb9640244478
User Name <usermail@gmail.com>
2026-01-17 17:45:44 +0530

    remove confirm pwd and forgot pwd

--- static/script.js
+++ static/script.js
@@ -1,28 +1,9 @@
 const register = document.getElementById('registerDiv');
 const login = document.getElementById('loginDiv');
-const forgotpwd = document.getElementById('forgotDiv');
 
 const reglink = document.getElementById('register');
-const pwdlink = document.getElementById('pwd');
-const authReq = document.getElementById('authentication');
-const otpReq = document.getElementById('requestOTP')
-const pwdButton = document.getElementById('pwdButton');
 
 reglink.addEventListener('click', function(){
     login.style.display = 'none';
-    forgotpwd.style.display = 'none';
     register.style.display = 'flex';
-});
-
-pwdlink.addEventListener('click', function(){
-    login.style.display = 'none';
-    register.style.display = 'none';
-    forgotpwd.style.display = 'flex';
-    authReq.style.display = 'none';
-});
-
-pwdButton.addEventListener('click', function(event){
-    event.preventDefault(); // prevents the page from going back to the login
-    otpReq.style.display = 'none';
-    authReq.style.display = 'flex';
 });
